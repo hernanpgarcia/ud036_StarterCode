32bdfee9a331c6b3e7d7af4a1fb171c9d5224398
hernanpgarcia <hernanpgarcia@gmail.com>
2017-08-08 20:13:52 -0300

    Re-download

--- media.py
+++ media.py
@@ -1,8 +1,12 @@
 import webbrowser
 
-class Movie():
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+class Movie ():
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
 
         self.title = movie_title
         self.storyline = movie_storyline
